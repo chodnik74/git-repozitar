1f9801c51aff3fefab939787bbd6f2276dc35679
Ondřej Ochodnický <ondrej.ochodnicky@burda.cz>
2018-04-26 18:57:02 +0200

    add comments

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+
+//NOTE: this is my program
 int main()
 {
    // printf() displays the string inside quotation
    printf("Hello, World!");
+
+   //return function
    return 0;
 }
